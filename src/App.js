cafcd733300d36a78e8621f441f5dd8bfb042aa6
Marta <rusin.ma@gmail.com>
2023-09-23 16:55:04 +0200

    moved backend call from App.js to GetUsers.js

--- src/App.js
+++ src/App.js
@@ -9,33 +9,12 @@ import GetUsers from './components/GetUsers/GetUsers';
 
 function App() {
 
-const [users, setUsers] = useState([]);
-const getUsers = async() => {
-try
-{
-  const response = await api.get("/users");
-  console.log(response.data);
-  setUsers(response.data);
-}
-catch(err)
-{
-  console.log(err);
-}
-
-}
-
-useEffect( () => {
-  getUsers();
-},[])
-
-
-
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home users={users} />} ></Route>
-          <Route path="/users" element={<GetUsers users={users} />} ></Route>
+          <Route path="/" element={<Home />} ></Route>
+          <Route path="/users" element={<GetUsers />} ></Route>
           
         </Route>
       </Routes>
--- src/components/GetUsers/GetUsers.js
+++ src/components/GetUsers/GetUsers.js
@@ -1,34 +1,42 @@
 import './GetUsers.css';
-import Carousel from 'react-material-ui-carousel';
-import { Paper } from '@mui/material';
 import { Table } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import api from '../../api/axiosConfig.js';
 
-const GetUsers = ({users}) => {
-  return (
-    <div>
-        <Table singleLine>
-        <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>First Name</Table.HeaderCell>
-                        <Table.HeaderCell>Last Name</Table.HeaderCell>
-                        <Table.HeaderCell>Age</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-            {
-                users.map(user =>{
-                    return(
+export default function Read() {
+
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        api.get("/users")
+            .then((response) => {
+                console.log(response.data)
+                setUsers(response.data);
+            })
+    }, []);
+
+    return (
+        <div>
+            <Table singleLine>
+            <Table.Header>
                         <Table.Row>
-                                <Table.Cell>{user.name}</Table.Cell>
-                                <Table.Cell>{user.surname}</Table.Cell>
-                                <Table.Cell>{user.age}</Table.Cell>
+                            <Table.HeaderCell>First Name</Table.HeaderCell>
+                            <Table.HeaderCell>Last Name</Table.HeaderCell>
+                            <Table.HeaderCell>Age</Table.HeaderCell>
                         </Table.Row>
-                    )
-                } )
-                   
-            }
-        </Table>
-    </div>
-  )
+                    </Table.Header>
+                {
+                    users.map(user =>{
+                        return(
+                            <Table.Row>
+                                    <Table.Cell>{user.name}</Table.Cell>
+                                    <Table.Cell>{user.surname}</Table.Cell>
+                                    <Table.Cell>{user.age}</Table.Cell>
+                            </Table.Row>
+                        )
+                    } )
+                    
+                }
+            </Table>
+        </div>
+    )
 }
-
-export default GetUsers
